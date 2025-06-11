8b4e038f798f7463b1cbf48d2a49627a18de0565
rollbox <brandadministration@gmail.com>
2025-06-11 16:57:36 +0100

    Update client.ts

--- src/integrations/supabase/client.ts
+++ src/integrations/supabase/client.ts
@@ -2,8 +2,8 @@
 import { createClient } from '@supabase/supabase-js';
 import type { Database } from './types';
 
-const SUPABASE_URL = "https://airgbbeaaavckmbttaqp.supabase.co";
-const SUPABASE_PUBLISHABLE_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImFpcmdiYmVhYWF2Y2ttYnR0YXFwIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDgzMzk0MTMsImV4cCI6MjA2MzkxNTQxM30.XkA_dlzxb6phEp5iNy9EGBooo2LowTFgrGVKwY-hSPI";
+const SUPABASE_URL = import.meta.env.VITE_SUPABASE_URL;
+const SUPABASE_ANON_KEY = import.meta.env.VITE_SUPABASE_ANON_KEY;
 
 // Import the supabase client like this:
 // import { supabase } from "@/integrations/supabase/client";
