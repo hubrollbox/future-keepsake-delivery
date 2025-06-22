07a8d1e2ae28b0fda3f54a80a464d59a69f8a8fa
rollbox <brandadministration@gmail.com>
2025-06-22 19:20:24 +0100

    feat: add SpeedInsights for performance monitoring

--- src/main.tsx
+++ src/main.tsx
@@ -2,7 +2,10 @@ import { createRoot } from 'react-dom/client'
 import App from './App.tsx'
 import './index.css'
 import { initGA } from './lib/analytics';
+import { SpeedInsights } from "@vercel/speed-insights/react"
 
 initGA();
 
 createRoot(document.getElementById("root")!).render(<App />);
+
+<SpeedInsights />
